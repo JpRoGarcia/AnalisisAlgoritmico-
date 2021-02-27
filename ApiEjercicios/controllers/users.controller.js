8dee8ff79ed011227e85896242321d64ab59cb0a
Juan Pablo Rojas García <juan.1020405@gmail.com>
2021-02-26 19:24:49 -0500

    - Instalacion de Nodemon y pg

---
+++ ApiEjercicios/controllers/users.controller.js
@@ -0,0 +1,8 @@
+const _pg = require('../services/postgres.service') 
+
+const getUsers = (req, res) => {
+    //_pg.execute()
+    return res.send("Oliwui UwU :V XD - Usuario")
+}
+
+module.exports = {getUsers};
